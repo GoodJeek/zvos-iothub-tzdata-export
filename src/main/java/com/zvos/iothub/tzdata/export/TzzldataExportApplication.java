e9513b0fa9b669f79a543251af268e10744ad211
lukelucklook <lupeijie2008@126.com>
2020-02-19 16:54:32 +0800

    tzdata-import

---
+++ src/main/java/com/zvos/iothub/tzdata/export/TzzldataExportApplication.java
@@ -0,0 +1,13 @@
+package com.zvos.iothub.tzdata.export;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TzzldataExportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TzzldataExportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zvos/iothub/zvos_iothub_tzdata_import/ZvosIothubTzdataImportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zvos.iothub.zvos_iothub_tzdata_import;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZvosIothubTzdataImportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
